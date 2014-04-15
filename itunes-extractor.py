fe1eb62b7a59a8be167ba57cae2dd3b55ccb1789
beallio <beallio@beallio.com>
2014-04-15 03:12:35 -0700

    Initial commit

--- itunes-extractor.py
+++ itunes-extractor.py
@@ -1,17 +1,3 @@
-"""
-  iTunes Podcast URL Extractor
-  php version implemented at http://itunes.so-nik.com
-  Original code & inspiration from http://itunes.so-nik.com website; SOURCE: http://snipplr.com/view/52465
-
-  Ex: http://ax.phobos.apple.com.edgesuite.net/WebObjects/MZStore.woa/wa/viewPodcast?id=269238657
-  Ex: http://itunes.apple.com/us/podcast/hombre-potato/id319810190?uo=4
- 
-    Usage: python itunes-extractor.py http://itunes.apple.com/us/podcast/the-morning-stream/id414564832?uo=4
-    Returns: http://myextralife.com/ftp/radio/morningstream.xml
-"""
-
-#============================================================================================================
-
 import argparse
 import sys
 from bs4 import BeautifulSoup
